e6e604e7126bd00bb251adc1f355600936aa562f
Naveeth Poojari <naveethpoojari@Naveeths-MacBook-Air.local>
2025-05-25 10:56:38 +0100

    Adds main.go file

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/Chatbot/handler"
+	"github.com/Chatbot/logger"
+	"github.com/Chatbot/middleware"
+	"github.com/Chatbot/service"
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
+
+func main() {
+	logger.Init()
+	e := echo.New()
+
+	middleware.Logger(e)
+
+	botService := service.NewBotService()
+	chatHandler := handler.NewChatbotHandler(botService)
+
+	e.POST("/chat", func(c echo.Context) error {
+		resp, err := chatHandler.HandleChat(c)
+		if err != nil {
+			return err
+		}
+		return c.JSON(http.StatusOK, resp)
+	})
+
+	e.Logger.Print("Starting GoBot on http://localhost:8080")
+	e.Logger.Fatal(e.Start(":8080"))
+}
